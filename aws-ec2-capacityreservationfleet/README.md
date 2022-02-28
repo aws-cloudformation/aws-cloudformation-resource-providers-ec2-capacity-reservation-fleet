5c6abd9c9bce60b53c1ff0d7c50f9701ae8dd2db
Vaibhav Chincholkar <vaibchin@amazon.com>
2022-02-28 11:08:30 -0800

    Adding provider implementation

---
+++ aws-ec2-capacityreservationfleet/README.md
@@ -0,0 +1,69 @@
+# AWS::EC2::CapacityReservationFleet
+
+Congratulations on starting development! Next steps:
+
+1. Write the JSON schema describing your resource, `aws-ec2-capacityreservationfleet.json`
+1. Implement your resource handlers.
+
+The RPDK will automatically generate the correct resource model from the schema whenever the project is built via Maven. You can also do this manually with the following command: `cfn generate`.
+
+> Please don't modify files under `target/generated-sources/rpdk`, as they will be automatically overwritten.
+
+The code uses [Lombok](https://projectlombok.org/), and [you may have to install IDE integrations](https://projectlombok.org/setup/overview) to enable auto-complete for Lombok-annotated classes.
+
+## Testing locally
+1. Start docker and enable file sharing for the workspace (No need to launch the container or app)
+2. Start sam local lambda in the resource directory in a new terminal `sam local start-lambda`
+3. Call the handler eg `cfn invoke CREATE local-tests/create-payload.json`
+
+Note : The log output will be shown in the sam lambda terminal
+
+## Testing in your aws account
+1. Build package `mvn package`
+2. Upload the build to your aws account `cfn submit --set-default --no-role`
+3. Create CFN stack with capacity reservation resource
+4. Let the stack run it’s cycle
+5. You could see the logs in the cloud watch log group
+
+NOTE : You could also specify the region while submitting  `cfn submit --region us-east-1 --set-default --no-role`
+
+
+### List Resource Registrations
+
+```
+aws cloudformation list-type-versions --type RESOURCE --type-name AWS::EC2::CapacityReservationFleet --region us-east-1
+```
+
+If you've run the `cfn submit` command multiple times, there should be multiple results from this api, with one of the results the `default`
+
+### Deregister Resource
+
+If you're done testing, and would like your cloudformation commands to be handled by the official, published version of the resource handler, you will have to deregister any development resource handler versions:
+
+First deregister all non-default versions (version id should be in the `list-type-versions` output):
+```
+aws cloudformation deregister-type --type RESOURCE --type-name AWS::EC2::CapacityReservationFleet --region us-east-1 --version-id 00000006
+```
+
+Then, when only the default version is left:
+
+```
+aws cloudformation deregister-type --type RESOURCE --type-name AWS::EC2::CapacityReservationFleet --region us-east-1
+```
+
+### Running contract tests
+
+``NOTE : update AvailabilityZone in inputs/<file>.json from {{ExportedAvailabilityZone}} to the desired one``
+
+Start local lambda
+```
+sam local start-lambda
+```
+Run tests
+```
+cfn test --enforce-timeout 240
+```
+Run individual tests
+```
+cfn test --enforce-timeout 240 -- -k contract_update_without_create
+```
---
+++ aws-ec2-capacityreservationfleet/src/main/java/software/amazon/ec2/capacityreservationfleet/UpdateHandler.java
@@ -0,0 +1,85 @@
+package software.amazon.ec2.capacityreservationfleet;
+
+import software.amazon.awssdk.awscore.exception.AwsErrorDetails;
+import software.amazon.awssdk.awscore.exception.AwsServiceException;
+import software.amazon.awssdk.services.ec2.Ec2Client;
+import software.amazon.awssdk.services.ec2.model.CapacityReservationFleet;
+import software.amazon.awssdk.services.ec2.model.CapacityReservationFleetState;
+import software.amazon.awssdk.services.ec2.model.DescribeCapacityReservationFleetsRequest;
+import software.amazon.awssdk.services.ec2.model.DescribeCapacityReservationFleetsResponse;
+import software.amazon.awssdk.services.ec2.model.ModifyCapacityReservationFleetResponse;
+import software.amazon.cloudformation.proxy.AmazonWebServicesClientProxy;
+import software.amazon.cloudformation.proxy.Logger;
+import software.amazon.cloudformation.proxy.ProgressEvent;
+import software.amazon.cloudformation.proxy.ProxyClient;
+import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
+
+public class UpdateHandler extends BaseHandlerStd {
+    private Logger logger;
+
+    protected ProgressEvent<ResourceModel, software.amazon.ec2.capacityreservationfleet.CallbackContext> handleRequest(
+        final AmazonWebServicesClientProxy proxy,
+        final ResourceHandlerRequest<ResourceModel> request,
+        final CallbackContext callbackContext,
+        final ProxyClient<Ec2Client> proxyClient,
+        final Logger logger) {
+        this.logger = logger;
+
+        return ProgressEvent.progress(request.getDesiredResourceState(), callbackContext)
+            .then(progress ->
+                proxy.initiate("AWS-EC2-CapacityReservationFleet::Update-exist", proxyClient, progress.getResourceModel(), progress.getCallbackContext())
+                        .translateToServiceRequest((model) -> Translator.translateToReadRequest(model, logger))
+                        .makeServiceCall((describeRequest, ec2ClientProxyClient) -> describeCapacityReservationFleets(describeRequest, ec2ClientProxyClient, logger))
+                        .handleError((awsRequest, exception, client, model, context) -> handleDescribeCapacityReservationFleetsError(awsRequest, exception, proxyClient, model, context))
+                        .done((describeFleetsRequest, describeFleetsResponse, client, model, context) ->
+                                Translator.translateToResourceFoundProgress(describeFleetsResponse, logger, context, model)))
+            .then(progress ->
+                proxy.initiate("AWS-EC2-CapacityReservationFleet::Update-update", proxyClient, progress.getResourceModel(), progress.getCallbackContext())
+                        .translateToServiceRequest((model) -> Translator.translateToUpdateRequest(model, logger))
+                        .makeServiceCall((awsRequest, ec2ClientProxyClient) -> {
+                            ModifyCapacityReservationFleetResponse response = null;
+                            try {
+                                logger.log(String.format("[INFO] Calling modifyCapacityReservationFleet for update with request: %s", request));
+                                response = ec2ClientProxyClient.injectCredentialsAndInvokeV2(awsRequest, ec2ClientProxyClient.client()::modifyCapacityReservationFleet);
+                                logger.log(String.format("[INFO] modifyCapacityReservationFleet response: %s", response));
+
+                                if (!response.returnValue()) {
+                                    logger.log(String.format("[ERROR] ModifyCapacityReservationFleet request failed. crFleetId: %s", awsRequest.capacityReservationFleetId()));
+                                    throw AwsServiceException.builder().awsErrorDetails(AwsErrorDetails.builder()
+                                            .errorMessage("ModifyCapacityReservationFleet failed.").build()).statusCode(500).build();
+                                }
+
+                                return response;
+                            } catch (final AwsServiceException e) {
+                                logger.log(String.format("[INFO] Exception thrown while modifying fleet request: %s", e));
+                                throw e;
+                            }
+                        })
+                        .stabilize((awsRequest, awsResponse, client, model, context) -> {
+                            boolean stabilized = false;
+                            try {
+                                final DescribeCapacityReservationFleetsRequest describeCapacityReservationFleetsRequest = Translator.translateToReadRequest(model, logger);
+                                final DescribeCapacityReservationFleetsResponse describeCapacityReservationFleetsResponse = describeCapacityReservationFleets(describeCapacityReservationFleetsRequest, proxyClient, logger);
+
+                                if (describeCapacityReservationFleetsResponse.hasCapacityReservationFleets()) {
+                                    final CapacityReservationFleet crFleet = describeCapacityReservationFleetsResponse.capacityReservationFleets().get(0);
+                                    stabilized = (CapacityReservationFleetState.ACTIVE.equals(crFleet.state()) || CapacityReservationFleetState.PARTIALLY_FULFILLED.equals(crFleet.state()));
+                                    logger.log(String.format("[INFO] Modified cr fleet is in %s state. Stabilized: ", crFleet.state(), stabilized));
+                                }
+
+                                return stabilized;
+                            } catch (final AwsServiceException ex) {
+                                logger.log(String.format("[ERROR] A exception occurred during stabilization: %s", ex));
+
+                                if (isUnauthorizedException(ex)) {
+                                    logger.log(String.format("[INFO] User is missing permissions for DescribeCapacityReservationFleets during Update."));
+                                }
+
+                                throw ex;
+                            }
+                        })
+                        .handleError((awsRequest, exception, client, model, context) -> Translator.translateToFailure(exception))
+                        .progress())
+            .then(progress -> new ReadHandler().handleRequest(proxy, request, callbackContext, proxyClient, logger));
+    }
+}
